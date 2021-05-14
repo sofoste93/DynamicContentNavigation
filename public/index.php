5397c7ef6e99732424f94123bc385f97a6b55c7a
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-14 14:08:27 +0200

    Adding page content

--- public/index.php
+++ public/index.php
@@ -8,6 +8,15 @@
 
   <div id="page">
 
+      <?php
+      // Show the homepage
+      // The homepage content could:
+      // * be static content (here or in a shared file)
+      // * show the first page from the nav
+      // * be in the database but add code to hide in the nav
+        include(SHARED_PATH . '/static_homepage.php');
+      ?>
+
   </div>
 
 </div>
